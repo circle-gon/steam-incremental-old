dcfc6dd3b70683f9376d01310a6720a23ec37a6d
octograp <drew01547@gmail.com>
2022-03-14 17:29:22 -0400

    Updated vue-tsc's support for watch mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { fileURLToPath, URL } from "url";
-import checker from "vite-plugin-checker";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'url';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
+
+// eslint: ran by checker
+// pretty: builtin to stackblitz editor
+// vue-tsc: supports watch mode but doesn't have checker support yet
 export default defineConfig({
   plugins: [
     vue(),
@@ -11,16 +15,17 @@ export default defineConfig({
       ...checker({
         typescript: true,
         eslint: {
-          lintCommand: "eslint . --ext .vue,.cjs,.ts --fix",
+          lintCommand: 'eslint . --ext .vue,.cjs,.ts --fix',
         },
+        //vueTsc: true
       }),
-      apply: "serve",
+      apply: 'serve',
     },
   ],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  base: "",
+  base: '',
 });
